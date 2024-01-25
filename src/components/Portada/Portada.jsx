b567fdc78875f3b6600edda7af80c68b592aea56
Danielpe <Danielpe_24@hotmail.com>
2024-01-24 20:49:05 -0500

    UPDATE: styles

--- src/components/Portada/Portada.jsx
+++ src/components/Portada/Portada.jsx
@@ -2,12 +2,12 @@ import "./Portada.css"
 
 const Portada = () => {
   return (
-    <>
-    <div className="portada">
-      <img src="/img/logo.png" alt="" />
-      <h2 className='titulo'>¡No te quedes sin la fach@ este verano !</h2>
+
+    <div className="portada bg-portada bg-cover min-h-[calc(102vh-108px)] flex items-center justify-center">
+      <img className="ml-[900px] mt-36" src="/img/logo.png" alt="" />
+      <h2 className="text-white text-4xl font-extralight font-arial absolute bottom-0 mb-[120px]">¡No te quedes sin la fach@ este verano !</h2>
     </div>
-    </>
+
   )
 }
 export default Portada
