72a1087912864e7a2eed1e01533afaa66d78f524
Danielpe <Danielpe_24@hotmail.com>
2024-01-24 19:52:48 -0500

    ADD: root project

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css'
+import NavBar from './components/NavBar/NavBar'
+import Portada from './components/Portada/Portada'
+import Marcas from './components/Marcas/Marcas'
+import CartWidget from './components/CardWidget/CardWidget'
+
+
+function App() {
+  return (
+    <>
+
+      <NavBar/>
+
+      <Portada/>
+
+      <Marcas/>
+
+      <CartWidget/>
+
+    </>
+  )
+}
+
+export default App
+
