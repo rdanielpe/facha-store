fbeaa442ef73468865701cb59e836afd6862b10a
Danielpe <Danielpe_24@hotmail.com>
2024-02-13 22:05:59 -0500

    ADD: home components

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,14 @@
+import ItemListContainer from "../ItemListContainer/ItemListContainer"
+import Portada from "../Portada/Portada"
+import Marcas from "../Marcas/Marcas"
+
+const Home = () => {
+  return (
+    <>
+      <Portada />
+      <Marcas />
+      <ItemListContainer />
+    </>
+  )
+}
+export default Home
