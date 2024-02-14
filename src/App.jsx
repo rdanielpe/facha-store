65a3d292675740de056d2ace405be3a945c30e6c
Danielpe <Danielpe_24@hotmail.com>
2024-02-13 22:06:08 -0500

    ADD: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import './App.css'
-import NavBar from './components/NavBar/NavBar'
-import Portada from './components/Portada/Portada'
-import Marcas from './components/Marcas/Marcas'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 
 function App() {
   return (
-    <>
-
-      <NavBar/>
-
-      <Portada/>
-
-      <Marcas/>
-
-      <ItemListContainer greeting={"LO MAS VENDIDO"}/>
-
-    </>
-  )
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/catalogo" element={<ItemListContainer greeting={"CATALOGO"} />} />
+        <Route exact path="/category/:categoryId" element={<Category />} />
+        <Route exact path="/item/:id" element={<ItemDetailContainer />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
-
+export default App;
