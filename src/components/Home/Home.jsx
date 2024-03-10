28991f5095ecdec622507688c74e5f2f5ab474a6
Danielpe <Danielpe_24@hotmail.com>
2024-03-10 00:35:41 -0500

    FIX: import list container

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,12 @@
-import ItemListContainer from "../ItemListContainer/ItemListContainer"
 import Portada from "../Portada/Portada"
 import Marcas from "../Marcas/Marcas"
 
 const Home = () => {
   return (
     <>
+
       <Portada />
       <Marcas />
-      <ItemListContainer />
     </>
   )
 }
