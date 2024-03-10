79a66f83e331921e501da79d1b787d6925aef1d2
Danielpe <Danielpe_24@hotmail.com>
2024-03-10 00:33:47 -0500

    ADD: firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN ,
+  projectId: import.meta.env.VITE_PROJECTID ,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET ,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID ,
+  appId: import.meta.env.VITE_APPID ,
+  measurementId:import.meta.env.VITE_MEASUREMENTID
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+//LA INSTANCIA DE LA BASE DE DATOS
+export const db = getFirestore(app);
