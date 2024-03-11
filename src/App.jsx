3bf7412aaace5823a6cf014cb45e5d5ce3ca06aa
Danielpe <Danielpe_24@hotmail.com>
2024-03-10 19:33:10 -0500

    ADD: cart provider

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,23 @@ import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import Home from "./components/Home/Home";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import Footer from "./components/Footer/Footer";
+// import Cart from "./components/Cart/Cart"
+import CartProvider from "./context/CartContext";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavBar />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/category/:categoryId" element={<ItemListContainer />} />
-        <Route exact path="/detalle/:id" element={<ItemDetailContainer />} />
-      </Routes>
-      <Footer />
+      <CartProvider>
+        <NavBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/category/:categoryId" element={<ItemListContainer />} />
+          <Route exact path="/all" element={<ItemListContainer />} />
+          <Route exact path="/detalle/:id" element={<ItemDetailContainer />} />
+          {/* <Route exact path="/cart" element={<Cart />} /> */}
+        </Routes>
+        <Footer />
+      </CartProvider>
     </BrowserRouter>
   );
 }
