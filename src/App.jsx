ce540b68804c46e93b8985fee7ab11ec6c659f82
Danielpe <Danielpe_24@hotmail.com>
2024-03-11 12:42:09 -0500

    ADD: cart and checkout

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import Cart from "./components/Cart/Cart";
 import Home from "./components/Home/Home";
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer";
 import Footer from "./components/Footer/Footer";
+import Checkout from "./components/Checkout/Checkout";
 // import Cart from "./components/Cart/Cart"
 import CartProvider from "./context/CartContext";
 
@@ -17,7 +19,9 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route exact path="/category/:categoryId" element={<ItemListContainer />} />
           <Route exact path="/all" element={<ItemListContainer />} />
+          <Route path="/cart" element={<Cart/>} />
           <Route exact path="/detalle/:id" element={<ItemDetailContainer />} />
+          <Route path="/checkout" element={<Checkout/>} />
           {/* <Route exact path="/cart" element={<Cart />} /> */}
         </Routes>
         <Footer />
