8d12df30566e2bf6970cddd60288e1a28a785c90
Danielpe <Danielpe_24@hotmail.com>
2024-03-10 19:31:57 -0500

    FIX: Detail products

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,9 +1,33 @@
+import {useState,useEffect} from 'react'
+import ItemDetail from '../ItemDetail/ItemDetail';
 import { useParams } from "react-router-dom"
+import { db } from "../../firebase/config";
+import { doc,getDoc } from 'firebase/firestore';
 
 const ItemDetailContainer = () => {
-  const { id } = useParams()
+  const [producto,setProducto] = useState([]);
+
+  const {id} = useParams()
+
+    useEffect(()=>{
+
+      //GENERAMOS EL LLAMADO AL DOCUMENTO DETERMINADO
+      const nuevoDoc = doc(db,"productos",id)
+
+      //HACEMOS EL LLAMADO AL DOCUMENTO Y LO RENDERIZAMOS EN PANTALLA
+      getDoc(nuevoDoc)
+      .then(res => {
+        if (res.exists()) {
+          setProducto({id: res.id, ...res.data() })
+        }
+      })
+      .catch(error => console.log(error))
+  },[])
+
   return (
-    <div>Detalle de producto: {id}</div>
+    <div>
+      <ItemDetail producto={producto}/>
+    </div>
   )
 }
 export default ItemDetailContainer
